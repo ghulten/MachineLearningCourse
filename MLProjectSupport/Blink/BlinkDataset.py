f3a8ae614f1e60341e6b1c75067fb1265c71a9bf
Geoff Hulten <ghulten@gmail.com>
2020-08-22 13:02:32 -0700

    Blink Data

---
+++ MLProjectSupport/Blink/BlinkDataset.py
@@ -0,0 +1,52 @@
+import collections
+import os
+import random
+
+
+def LoadRawData(path, includeLeftEye = True, includeRightEye = True, shuffle=True):
+    xRaw = []
+    yRaw = []
+    
+    if includeLeftEye:
+        closedEyeDir = os.path.join(path, "closedLeftEyes")
+        for fileName in os.listdir(closedEyeDir):
+            if fileName.endswith(".jpg"):
+                xRaw.append(os.path.join(closedEyeDir, fileName))
+                yRaw.append(1)
+
+        openEyeDir = os.path.join(path, "openLeftEyes")
+        for fileName in os.listdir(openEyeDir):
+            if fileName.endswith(".jpg"):
+                xRaw.append(os.path.join(openEyeDir, fileName))
+                yRaw.append(0)
+
+    if includeRightEye:
+        closedEyeDir = os.path.join(path, "closedRightEyes")
+        for fileName in os.listdir(closedEyeDir):
+            if fileName.endswith(".jpg"):
+                xRaw.append(os.path.join(closedEyeDir, fileName))
+                yRaw.append(1)
+
+        openEyeDir = os.path.join(path, "openRightEyes")
+        for fileName in os.listdir(openEyeDir):
+            if fileName.endswith(".jpg"):
+                xRaw.append(os.path.join(openEyeDir, fileName))
+                yRaw.append(0)
+
+    if shuffle:
+        random.seed(1000)
+
+        index = [i for i in range(len(xRaw))]
+        random.shuffle(index)
+
+        xOrig = xRaw
+        xRaw = []
+
+        yOrig = yRaw
+        yRaw = []
+
+        for i in index:
+            xRaw.append(xOrig[i])
+            yRaw.append(yOrig[i])
+
+    return (xRaw, yRaw)
